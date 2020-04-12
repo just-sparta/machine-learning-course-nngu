8e20e62cec2cc33be27d4fb37b8a8702dab02cba
Victor Kuznetsov <32412802+just-sparta@users.noreply.github.com>
2020-04-12 18:57:05 +0300

    Add files via upload

--- unn_scripts_lab/unit_tests.py
+++ unn_scripts_lab/unit_tests.py
@@ -84,10 +84,36 @@ class TestPolynomial(unittest.TestCase):
         p2 = polynomial.Polynomial(p1)
         self.assertEqual(p1, p2)
 
+    def test_copy_polynom_tuple(self):
+        p1 = polynomial.Polynomial(1, 2, -3)
+        p2 = polynomial.Polynomial(p1)
+        self.assertEqual(p1, p2)
+
+    def test_copy_polynom_edit_coeffs(self):
+        p1 = polynomial.Polynomial([1, 2, 4])
+        p2 = polynomial.Polynomial(p1)
+        p2.coefficients[0] = 100
+        self.assertNotEqual(p1, p2)
+
     def test_neg_polynom(self):
         p1 = polynomial.Polynomial([1, 2, -3])
         p2 = p1.__neg__()
         self.assertEqual(p1 * (-1), p2)
+
+    def test_neg_polynom_2(self):
+        p1 = polynomial.Polynomial([1, 2, -3])
+        p2 = -p1
+        self.assertEqual(p1 * (-1), p2)
+
+    def test_const_sub_left(self):
+        p1 = polynomial.Polynomial([1, 2, 3])
+        p2 = 1
+        self.assertEqual((p1 - p2).coefficients, [1, 2, 2])
+
+    def test_const_sub_right(self):
+        p1 = polynomial.Polynomial([1, 2, 3])
+        p2 = 1
+        self.assertEqual((p2 - p1).coefficients, [-1, -2, -2])
 
     def test_derivative(self):
         p1 = polynomial.Polynomial([1, 2, -3])
@@ -97,22 +123,109 @@ class TestPolynomial(unittest.TestCase):
     def test_string(self):
         p1 = polynomial.Polynomial([1, 1, 1, 1, 1])
         p2 = p1.__str__()
-        self.assertEqual('x^4 + x^3 + x^2 + x + 1', p2)
+        self.assertEqual('x^4+x^3+x^2+x+1', p2)
 
     def test_string_tuple(self):
         p1 = polynomial.Polynomial(1, 1, 1, 1, 1)
         p2 = p1.__str__()
-        self.assertEqual('x^4 + x^3 + x^2 + x + 1', p2)
+        self.assertEqual('x^4+x^3+x^2+x+1', p2)
 
     def test_string_2(self):
         p1 = polynomial.Polynomial([5, 5, -5, -5, 1])
         p2 = p1.__str__()
-        self.assertEqual('5x^4 + 5x^3 - 5x^2 - 5x + 1', p2)
+        self.assertEqual('5x^4+5x^3-5x^2-5x+1', p2)
 
     def test_string_2_tuple(self):
         p1 = polynomial.Polynomial(5, 5, -5, -5, 1)
         p2 = p1.__str__()
-        self.assertEqual('5x^4 + 5x^3 - 5x^2 - 5x + 1', p2)
+        self.assertEqual('5x^4+5x^3-5x^2-5x+1', p2)
+
+    def test_add_polynom_null(self):
+        p1 = polynomial.Polynomial([0, 0, 1, 2, -3])
+        p2 = polynomial.Polynomial([1, 2])
+        self.assertEqual(p1 + p2, polynomial.Polynomial([1, 3, -1]))
+
+    def test_add_polynom_tuple_null(self):
+        p1 = polynomial.Polynomial(0, 0, 1, 2, -3)
+        p2 = polynomial.Polynomial(1, 2)
+        self.assertEqual(p1 + p2, polynomial.Polynomial(1, 3, -1))
+
+    def test_add_const_null(self):
+        p1 = polynomial.Polynomial([0, 0, 1, 2, -3])
+        p2 = 1
+        self.assertEqual(p1 + p2, polynomial.Polynomial([1, 2, -2]))
+
+    def test_add_const_tuple_null(self):
+        p1 = polynomial.Polynomial(0, 0, 1, 2, -3)
+        p2 = 1
+        self.assertEqual(p1 + p2, polynomial.Polynomial(1, 2, -2))
+
+    def test_sub_polynom_null(self):
+        p1 = polynomial.Polynomial([0, 0, 1, 2, -3])
+        p2 = polynomial.Polynomial([1, 2])
+        self.assertEqual(p1 - p2, polynomial.Polynomial([1, 1, -5]))
+
+    def test_sub_polynom_tuple_null(self):
+        p1 = polynomial.Polynomial(0, 0, 1, 2, -3)
+        p2 = polynomial.Polynomial(1, 2)
+        self.assertEqual(p1 - p2, polynomial.Polynomial(1, 1, -5))
+
+    def test_sub_const_null(self):
+        p1 = polynomial.Polynomial([0, 0, 1, 2, -3])
+        p2 = 1
+        self.assertEqual(p1 + p2, polynomial.Polynomial([1, 2, -2]))
+
+    def test_sub_const_tuple_null(self):
+        p1 = polynomial.Polynomial(0, 0, 1, 2, -3)
+        p2 = 1
+        self.assertEqual(p1 + p2, polynomial.Polynomial(1, 2, -2))
+
+    def test_mul_polynom_null(self):
+        p1 = polynomial.Polynomial([0, 0, 1, 2, -3])
+        p2 = polynomial.Polynomial([1, 2])
+        self.assertEqual(p1 * p2, polynomial.Polynomial([1, 4, 1, -6]))
+
+    def test_mul_polynom_tuple_null(self):
+        p1 = polynomial.Polynomial(0, 0, 1, 2, -3)
+        p2 = polynomial.Polynomial(1, 2)
+        self.assertEqual(p1 * p2, polynomial.Polynomial(1, 4, 1, -6))
+
+    def test_mul_const_null(self):
+        p1 = polynomial.Polynomial([0, 0, 1, 2, -3])
+        p2 = 1
+        self.assertEqual(p1 * p2, polynomial.Polynomial([1, 2, -3]))
+
+    def test_can_modify_coefficients(self):
+        p1 = polynomial.Polynomial([2, 2, 4])
+        p1.coefficients[0] -= 1
+        p1.coefficients[1] -= 1
+        self.assertEqual(p1, polynomial.Polynomial([1, 1, 4]))
+
+    def test_string_3(self):
+        self.assertEqual('-1', str(polynomial.Polynomial([-1])))
+
+    def test_string_3_with_zero(self):
+        self.assertEqual('0', str(polynomial.Polynomial([])))
+
+    def test_zero_mul(self):
+        p1 = polynomial.Polynomial([1, 4, 5])
+        p2 = 0
+        self.assertEqual((p2 * p1).coefficients, [0])
+
+    def test_mul_with_empty_polinomial(self):
+        p1 = polynomial.Polynomial([])
+        p2 = 5
+        self.assertEqual((p2 * p1).coefficients, [0])
+
+    def test_coefficients(self):
+        coefficients = [1, 1, 3]
+        p1 = polynomial.Polynomial(coefficients)
+        self.assertEqual(coefficients, p1.coefficients)
+
+    def test_coefficients_tuple(self):
+        coefficients = (1, 1, 3)
+        p1 = polynomial.Polynomial(coefficients)
+        self.assertEqual(list(coefficients), p1.coefficients)
 
 
 if __name__ == '__main__':
